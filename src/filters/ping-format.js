d03413b0c4c47956cdf93a4815f30dd103300e11
Paulo Coutinho <paulo@prsolucoes.com>
2017-07-06 17:20:49 -0300

    migrated to vuejs

--- src/filters/ping-format.js
+++ src/filters/ping-format.js
@@ -1,35 +1,31 @@
+import {PING_TIME_HOURS, PING_TIME_MINUTES, PING_TIME_MS, PING_TIME_SECONDS} from "../utils/constants";
+
 export default function (value) {
 	if (!value) return '';
 
 	value = parseInt(value);
 
-	const TIME_MS = 1000;
-	const TIME_SECONDS = (TIME_MS * 60);
-	const TIME_MINUTES = (TIME_SECONDS * 60);
-	const TIME_HOURS = (TIME_MINUTES * 60);
-	const TIME_DAYS = (TIME_HOURS * 24);
-
 	let newValue = 0;
 	let text = "";
 
-	if (value < TIME_MS) {
+	if (value < PING_TIME_MS) {
 		newValue = value;
-		text = "m";
-	} else if (value < TIME_SECONDS) {
-		newValue = Math.floor(value / TIME_MS);
+		text = "ms";
+	} else if (value < PING_TIME_SECONDS) {
+		newValue = Math.floor(value / PING_TIME_MS);
 		text = "sec";
-	} else if (value < TIME_MINUTES) {
-		newValue = Math.floor(value / TIME_SECONDS);
+	} else if (value < PING_TIME_MINUTES) {
+		newValue = Math.floor(value / PING_TIME_SECONDS);
 		text = "minute";
-	} else if (value < TIME_HOURS) {
-		newValue = Math.floor(value / TIME_MINUTES);
+	} else if (value < PING_TIME_HOURS) {
+		newValue = Math.floor(value / PING_TIME_MINUTES);
 		text = "hour";
 	} else {
-		newValue = Math.floor(value / TIME_HOURS);
+		newValue = Math.floor(value / PING_TIME_HOURS);
 		text = "day";
 	}
 
-	if (newValue == 1) {
+	if (newValue === 1) {
 		return newValue + " " + text + " ago";
 	} else {
 		return newValue + " " + text + "s ago";
