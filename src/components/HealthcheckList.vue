174cf1b5c42fbf9e88d3e23ba281fd89a361db8d
Paulo Coutinho <paulo@prsolucoes.com>
2017-07-07 14:29:53 -0300

    migrated to vuejs

--- src/components/HealthcheckList.vue
+++ src/components/HealthcheckList.vue
@@ -25,7 +25,7 @@
 				</b-table-column>
 
 				<b-table-column field="status" label="Status" sortable>
-					<span v-html="props.row.status | hcStatusFormat"></span>
+					<span v-html="$options.filters.hcStatusFormat(props.row.status)"></span>
 				</b-table-column>
 
 				<b-table-column field="type" label="Value" width="200">
